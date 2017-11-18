1bd232097a2d43a44196dd1932fb022b353f1207
haxiomic <haxiomic@gmail.com>
2017-11-18 23:24:08 +0000

    Correct warp from r0 to r1

--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,101 @@
 let w = window.innerWidth;
 let h = window.innerHeight;
 
-let app = new PIXI.Application(w, h, { transparent: false, backgroundColor: 0xFF0000 });
+let app = new PIXI.Application(w, h, { transparent: false, backgroundColor: 0x00FF00 });
 document.body.appendChild(app.renderer.view);
-
-let stdDev = (<PIXI.WebGLRenderer>app.renderer).gl.getExtension('OES_standard_derivatives');
-console.log(stdDev);
 
 let stage = new PIXI.Container();
 
-// let texture = PIXI.Texture.fromImage('assets/test-image.jpg');
+// let texture = PIXI.Texture.fromImage('assets/test-map.jpg');
 let texture = PIXI.Texture.fromVideo('assets/planet-earth.mp4');
 let videoSprite = new PIXI.Sprite(texture);
-videoSprite.width = app.renderer.width;
-videoSprite.height = app.renderer.height;
+videoSprite.width = w;
+videoSprite.height = h;
 app.stage.addChild(videoSprite);
 
-let testMap = PIXI.Texture.fromImage('assets/uv.png');
+let testMap = PIXI.Texture.fromImage('assets/test-map.png');
 
 class VisionMapFilter extends PIXI.Filter<{
 	displacementMap: PIXI.Texture,
 	contrastMap: PIXI.Texture,
+	dimensions: {x:number, y:number},
+	filterArea: {x:number, y:number, z:number, w:number}
 }> {
 
+	private static vertexShader = `
+		attribute vec2 aVertexPosition;
+		attribute vec2 aTextureCoord;
+		uniform mat3 projectionMatrix;
+		uniform mat3 filterMatrix;
+		varying vec2 vTextureCoord;
+		varying vec2 vFilterCoord;
+		void main(void){
+		   gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+		   vFilterCoord = ( filterMatrix * vec3( aTextureCoord, 1.0)  ).xy;
+		   vTextureCoord = vFilterCoord;
+		}
+	`;
+
 	private static fragmentShader = `
-		#extension GL_OES_standard_derivatives : enable
 		precision highp float;
 
 		varying vec2 vTextureCoord;
 		uniform sampler2D uSampler;
-		uniform sampler2D displacementMap;
-		uniform sampler2D contrastMap;
+		uniform vec2 dimensions;
+		uniform vec4 filterArea;
 
-		uniform vec2 mapSize;
+		// uniform sampler2D displacementMap;
+		// uniform sampler2D contrastMap;
+
+		vec2 mapCoord( vec2 coord ){
+		    coord *= filterArea.xy;
+		    coord += filterArea.zw;
+		    return coord;
+		}
+
+		vec2 unmapCoord( vec2 coord ){
+		    coord -= filterArea.zw;
+		    coord /= filterArea.xy;
+		    return coord;
+		}
 
 		vec3 brightnessContrast(vec3 value, float brightness, float contrast){
 		    return (value - 0.5) * contrast + 0.5 + brightness;
 		}
 
 		void main(){
-			vec2 uv = vTextureCoord;
+			vec2 uv = mapCoord(vTextureCoord) / dimensions;
 
 			vec2 c = vec2(0.5);
 			vec2 d = c - uv;
 			float l = length(d);
 			vec2 n = normalize(d);
 
-			float m = 0.8 - l;
-			uv += n * m * m * m * 0.8;
+			const float r = 0.1;
+			const float R = 0.8;
 
-			vec3 col = texture2D(uSampler, uv).rgb;
+			//as x approaches r from R, f(x) approaches r
+			//at x = R, f(x) = 0
+			float dx = 1.0 - (l - r)/(R - r);
+			dx = step(0.0, dx) * dx; //mask out < 0
+			float boundsMask = step(dx, 1.0); // mask out > 1
+			float m = dx * boundsMask * r;
+
+// gl_FragColor = vec4(m, 0., 0., 1.); return;
+
+			vec2 delta = n * m;
+
+// gl_FragColor = vec4(delta, 0., 1.); return;
+
+			uv += delta;
+
+// gl_FragColor = vec4(uv, 0., 1.); return;
+
+			vec3 col = texture2D(uSampler, unmapCoord(uv*dimensions)).rgb;
+
+			// prevent image wrapping
+			// vec2 s = step(uv, vec2(1.0)) * step(vec2(0.0), uv);
+			// col *= s.x * s.y;
 
 			// brightness / contrast
 			// float cm = texture2D(contrastMap, uv).r;
@@ -60,13 +106,22 @@ class VisionMapFilter extends PIXI.Filter<{
 	`;
 
 	constructor(contrastMap: PIXI.Texture, displacementMap: PIXI.Texture){
-		super(VisionMapFilter.defaultVertexSrc, VisionMapFilter.fragmentShader);
+		super(VisionMapFilter.vertexShader, VisionMapFilter.fragmentShader);
 
-		this.uniforms.contrastMap = contrastMap;
-		this.uniforms.displacementMap = displacementMap;
+		this.uniforms.dimensions = {x: 0, y: 0};
+		this.uniforms.filterArea = {x: 0, y: 0, z: 0, w: 0};
+		// this.uniforms.contrastMap = contrastMap;
+		// this.uniforms.displacementMap = displacementMap;
 	}
 
 	apply(filterManager: PIXI.FilterManager, input: PIXI.RenderTarget, output: PIXI.RenderTarget){
+		this.uniforms.dimensions.x = input.sourceFrame.width
+		this.uniforms.dimensions.y = input.sourceFrame.height
+		this.uniforms.filterArea.x = output.size.width;
+		this.uniforms.filterArea.y = output.size.height;
+		this.uniforms.filterArea.z = input.sourceFrame.x;
+		this.uniforms.filterArea.w = input.sourceFrame.y;
+
 		filterManager.applyFilter(this, input, output);
 	}
 
